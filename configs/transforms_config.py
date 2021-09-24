4f7b2fba96b379d76f8f984ca4cc21c23b5120de
woocosmos <woocosmos@gmail.com>
2021-09-24 18:46:35 +0900

    zookiz transform 추가

--- configs/transforms_config.py
+++ configs/transforms_config.py
@@ -91,6 +91,30 @@ class SketchToImageTransforms(TransformsConfig):
 		}
 		return transforms_dict
 
+  ######## 추가
+class ZookizTransforms(TransformsConfig):
+
+	def __init__(self, opts):
+		super(ZookizTransforms, self).__init__(opts)
+
+	def get_transforms(self):
+		transforms_dict = {
+			'transform_gt_train': transforms.Compose([
+				transforms.Resize((256, 256)),
+				transforms.ToTensor(),
+				transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+			'transform_source': transforms.Compose([
+				transforms.Resize((256, 256)),
+				transforms.ToTensor()]),
+			'transform_test': transforms.Compose([
+				transforms.Resize((256, 256)),
+				transforms.ToTensor(),
+				transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+			'transform_inference': transforms.Compose([
+				transforms.Resize((256, 256)),
+				transforms.ToTensor()]),
+		}
+		return transforms_dict
 
 class SegToImageTransforms(TransformsConfig):
 
@@ -117,7 +141,6 @@ class SegToImageTransforms(TransformsConfig):
 				transforms.ToTensor()])
 		}
 		return transforms_dict
-
 
 class SuperResTransforms(TransformsConfig):
 
