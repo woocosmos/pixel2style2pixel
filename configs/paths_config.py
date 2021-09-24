9993ccdd57d185d62a93dac3bf9f0ef52b43f314
woocosmos <woocosmos@gmail.com>
2021-09-24 18:41:53 +0900

    모델 config 수정

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -6,15 +6,20 @@ dataset_paths = {
 	'celeba_train_segmentation': '',
 	'celeba_test_segmentation': '',
 	'ffhq': '',
+
+	'zookiz_sketch_train': './new_sketch_train',
+  	'zookiz_train': './new_target_train', 
+  	'zookiz_test_sktech': './new_sketch_test',
+  	'zookiz_test':'./new_target_test'
 }
 
 model_paths = {
-	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+   'stylegan_ffhq' : './pretrained_models/network-snapshot-003894.pt',
+	'ir_se50': './pretrained_models/model_ir_se50.pth',
+	'circular_face': './pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': './pretrained_models/pnet.npy',
+	'mtcnn_rnet': './pretrained_models/rnet.npy',
+	'mtcnn_onet': './pretrained_models/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
-	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+	# 'moco': '/content/drive/MyDrive/stylegan_exp01/try02/pretrained_models/moco_v2_800ep_pretrain.pt'
 }
